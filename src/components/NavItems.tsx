2bdb0a72c1c368a0ac2907560a3fbb6b44b7d12d
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-11-21 17:18:11 +0100

    nav-registration part

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,43 +1,64 @@
-"use client"
-import React,{useState} from 'react'
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+import NavItem from "./NavItem";
+import { PRODUCT_CATEGORIES } from "../config";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
-import NavItem from "./NavItem"
-import { PRODUCT_CATEGORIES } from '../config'
-
-interface Props {
-    
-}
-
-
+interface Props {}
 
 const NavItems = (props: Props) => {
-    //state for indexes
-    const [activeIndex, setActiveIndex] = useState<null | number>(null)
+  //state for indexes
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-    // if activeIndex <> null i know there is opedned category
-    const isAnyOpen = activeIndex !== null
+  // if activeIndex !== null i know there is opedned category
+  const isAnyOpen = activeIndex !== null;
 
-    return (
-        <div className="flex gap-4 h-full">
-            {PRODUCT_CATEGORIES.map((cat,index)=>{
-                const handleOpen = () => { // to track wich item in navbar is open
-                    console.log(activeIndex)
-                    if(activeIndex === index){
-                        setActiveIndex(null)
-                    }else{
-                        setActiveIndex(index)
-                    }
-                }
+  // if user clicks outside nav, it should close, laso hook is from internet :)
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef, () => setActiveIndex(null));
 
-                //it is opend when index of mapped item is activeIndex
-                const isOpen = index === activeIndex
+  //if user press escape it should close nav too :)
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
 
-                return(
-                    <NavItem key={cat.value} category={cat} handleOpen={handleOpen} isOpen={isOpen} isAnyOpen={isAnyOpen}/>
-                )
-            })}
-        </div>
-    )
-}
+    document.addEventListener("keydown", handler);
+    return () => {// simple clean-up
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
 
-export default NavItems
+  return (
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((cat, index) => {
+        const handleOpen = () => {
+          // to track wich item in navbar is open
+          console.log(activeIndex);
+          if (activeIndex === index) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(index);
+          }
+        };
+
+        //it is opend when index of mapped item is activeIndex
+        const isOpen = index === activeIndex;
+
+        return (
+          <NavItem
+            key={cat.value}
+            category={cat}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavItems;
