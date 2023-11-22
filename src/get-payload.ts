6517476ac2ae7a3df47211e251d3613fe586af17
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2023-11-22 14:00:22 +0100

    Update get-payload.ts

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,10 +2,23 @@ import dotenv from "dotenv";
 import path from "path";
 import payload from "payload";
 import type { InitOptions } from "payload/config";
+import nodemailer from "nodemailer"
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
+
+
+//this is for sending verification email
+const transporter = nodemailer.createTransport({
+  host:"smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth:{
+    user:"resend",
+    pass: process.env.RESEND_API_KEY
+  }
+})
 
 // use caching
 let cached = (global as any).payload;
@@ -22,7 +35,7 @@ interface Args {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({ initOptions }: Args) => {
+export const getPayloadClient = async ({ initOptions }: Args):Promise<Payload> => {
     if(!process.env.PAYLOAD_SECRET!){
         throw new Error("PAYLOAD_SECRET is missing!")
     }
@@ -31,9 +44,14 @@ export const getPayloadClient = async ({ initOptions }: Args) => {
     }
     if(!cached.promise){
         cached.promise = payload.init({
-            secret:process.env.PAYLOAD_SECRET!,
-            local:initOptions?.express ? false : true,
-            ...(initOptions || {}) // put everything else in :)
+          email:{
+            transport: transporter,
+            fromAddress: "onboarding@resend.dev",
+            fromName:"DigitalHippo",
+          },
+          secret:process.env.PAYLOAD_SECRET!,
+          local:initOptions?.express ? false : true,
+          ...(initOptions || {}) // put everything else in :)
         })
     }
 
