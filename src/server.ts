11969a0e61607ca0992f41b62a9f8674528bf30a
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2023-11-23 17:52:13 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
 import * as trpcExpress from "@trpc/server/adapters/express"
 import {appRouter} from "./trpc"
+import {inferAsyncReturnType} from "@trpc/server"
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -11,6 +12,9 @@ const createContext = ({req,res}:trpcExpress.CreateExpressContextOptions) => ({
   req,
   res,
 })
+
+//this to get types of context, very usefull whe you want to get something like token from context :)
+export type ExpressContext = inferAsyncReturnType<typeof createContext >
 
 const start = async () => {
   const payload = await getPayloadClient({
@@ -24,7 +28,7 @@ const start = async () => {
 
   app.use("/api/trpc",trpcExpress.createExpressMiddleware({
     router: appRouter,
-    createContext
+    createContext,
   }))
 
   app.use((req, res) => nextHandler(req, res));
