f9c6235bfc04a30c229eb0ebdbabb4d95477036c
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2023-11-23 17:51:57 +0100

    Update page.tsx

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -9,19 +9,40 @@ import Link from "next/link";
 import React from "react";
 import {useForm} from "react-hook-form"
 import {zodResolver} from "@hookform/resolvers/zod"
-
+import {useRouter} from "next/navigation"
 import {TAuthCredentialsValidator,AuthCredentialsValidator} from "@/lib/validators/account-credentials-validator"
 import {trpc} from "@/trpc/client"
+import {toast} from "sonner"
 
 
 
 const SignUpPage = () => {
+  const router = useRouter()
 
   const { register, handleSubmit, formState:{errors} } = useForm<TAuthCredentialsValidator>({
     resolver: zodResolver(AuthCredentialsValidator)
   })
 
-  const {mutate, isLoading} = trpc.auth.createPayloadUser.useMutation({})
+  const {mutate, isLoading} = trpc.auth.createPayloadUser.useMutation({
+    onError: (err) =>{
+      if(err.data?.code === "CONFLICT"){ //there is already user with this email
+        toast.error("This eamil is already in use. Sign in instead?")
+        return //this is to show up only one error
+      }
+
+      if(err instanceof ZodError){ // if user find the way to mess up inputs they will have correct toast
+        toast.error(err.issues[0].message)
+        return
+      }
+
+      // if error is something else that two cases in ifs
+      toast.error("Something went wrong. Please try again.")
+    },
+    onSuccess:({sentToEmail})=>{
+      toast.success(`Verificatin email sent to ${sentToEmail}.`)
+      router.push("/verify-email?to=" + sentToEmail)
+    },
+  })
 
   const onSubmitForm =  ({email,password}:TAuthCredentialsValidator) => {
     //send data to server
@@ -37,7 +58,7 @@ const SignUpPage = () => {
             <Icons.logo className="h-20 w-20" />
             <h1 className="text-2xl font-bold">Create an account</h1>
             <Link
-              href={"sign-in"}
+              href={"/sign-in"}
               className={buttonVariants({
                 variant: "link",
                 className: "gap-1.5",
@@ -55,12 +76,18 @@ const SignUpPage = () => {
                     <Input type="email" className={cn({
                         "focus-visible:ring-red-500": errors.email
                     })} placeholder="you@example.com" {...register("email")}/>
+                    {errors?.email && (
+                      <p className="text-sm text-red-500">{errors.email.message}</p>
+                    )}
                 </div>
                 <div className="grid gap-1 py-2">
                     <Label htmlFor="password">Password</Label>
                     <Input type="password" className={cn({
                         "focus-visible:ring-red-500": errors.password
                     })} placeholder="Password" {...register("password")}/>
+                      {errors?.password && (
+                      <p className="text-sm text-red-500">{errors.password.message}</p>
+                    )}
                 </div>
                 <Button>Sign up</Button>
               </div>
