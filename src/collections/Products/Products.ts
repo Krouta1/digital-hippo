28bc36201fde05bce7e4370760cad5aab1f7f75f
Petr Kroutil <pkroutil1@seznam.cz>
2023-11-24 21:36:27 +0100

    prov commit

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,127 +1,128 @@
 import { PRODUCT_CATEGORIES } from "../../config";
 import { CollectionConfig } from "payload/types";
 
-export const Products : CollectionConfig = {
-    slug: "products",
-    admin:{
-        useAsTitle : "name"
+export const Products: CollectionConfig = {
+  slug: "products",
+  admin: {
+    useAsTitle: "name",
+  },
+  access: {},
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
     },
-    access:{},
-    fields:[
+    {
+      name: "name",
+      label: "Name",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "description",
+      label: "Product details",
+      type: "textarea",
+    },
+    {
+      name: "price",
+      label: "Price in USD",
+      type: "number",
+      min: 0,
+      max: 1000,
+      required: true,
+    },
+    {
+      name: "category",
+      label: "Category",
+      type: "select",
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
+      required: true,
+    },
+    {
+      name: "product_files",
+      label: "Product file(s)",
+      type: "relationship",
+      required: true,
+      relationTo: "product_files",
+      hasMany: false,
+    },
+    {
+      name: "approvedForSale",
+      label: "Product Status",
+      type: "select",
+      defaultValue: "pending",
+      access: {
+        // only admin can CRUD
+        create: ({ req }) => req.user.role === "admin",
+        read: ({ req }) => req.user.role === "admin",
+        update: ({ req }) => req.user.role === "admin",
+      },
+      options: [
         {
-            name:"user",
-            type: "relationship",
-            relationTo: "users",
-            required: true,
-            hasMany: false,
-            admin:{
-                condition: () => false
-            }
+          label: "Pending Verification",
+          value: "pending",
         },
         {
-            name:"name",
-            label: "Name",
-            type: "text",
-            required: true,
+          label: "Approved",
+          value: "approved",
         },
         {
-            name: "description",
-            label:"Product details",
-            type: "textarea",
-
+          label: "Denied",
+          value: "denied",
         },
+      ],
+    },
+    {
+      name: "priceId",
+      access: {
+        // no one can change this...only inside code it can be done
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: "stripeId",
+      access: {
+        // no one can change this...only inside code it can be done
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: "images",
+      type: "array",
+      label: "Product images",
+      minRows: 1,
+      maxRows: 4,
+      required: true,
+      labels: {
+        singular: "Image",
+        plural: "Images",
+      },
+      fields: [
         {
-            name: "price",
-            label: "Price in USD",
-            type: "number",
-            min: 0,
-            max:1000,
-            required:true,
+          name: "image",
+          type: "upload",
+          relationTo: "media",
+          required: true,
         },
-        {
-            name:"category",
-            label:"Category",
-            type:"select",
-            options:PRODUCT_CATEGORIES.map(({label,value})=>({label,value})),
-            required:true,
-        },
-        // {
-        //     name:"product_files",
-        //     label:"Product file(s)",
-        //     type: "relationship",
-        //     required:true,
-        //     relationTo:"product_files",
-        //     hasMany: false,
-        // },
-        {
-            name:"approvedForSale",
-            label:"Product Status",
-            type:"select",
-            defaultValue:"pending",
-            access:{ // only admin can CRUD
-                create:({req})=> req.user.role === "admin",
-                read:({req})=> req.user.role === "admin",
-                update:({req})=> req.user.role === "admin",
-            },
-            options:[
-                {
-                    label: "Pending Verification",
-                    value: "pending",
-                },
-                {
-                    label: "Approved",
-                    value: "approved",
-                },
-                {
-                    label: "Denied",
-                    value: "denied",
-                },
-            ],
-
-        },
-        {
-            name: "priceId",
-            access:{ // no one can change this...only inside code it can be done
-                create: () =>false,
-                read: () =>false,
-                update: () =>false,
-            },
-            type:"text",
-            admin:{
-                hidden:true
-            }
-        },
-        {
-            name: "stripeId",
-            access:{ // no one can change this...only inside code it can be done
-                create: () =>false,
-                read: () =>false,
-                update: () =>false,
-            },
-            type:"text",
-            admin:{
-                hidden:true
-            }
-        },
-        {
-            name:"images",
-            type:"array",
-            label:"Product images",
-            minRows:1,
-            maxRows:4,
-            required:true,
-            labels:{
-                singular:"Image",
-                plural:"Images",
-            },
-            fields:[
-                {
-                    name:"image",
-                    type:"upload",
-                    relationTo:"media",
-                    required:true
-                },
-            ],
-        },
-    ],
-}
+      ],
+    },
+  ],
+};
