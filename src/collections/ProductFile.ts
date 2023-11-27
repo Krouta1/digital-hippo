8f4047a808d004c270647e803666b8e5e8d8023a
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-11-27 17:48:39 +0100

    Orders and ProductFiles collection finished

--- src/collections/ProductFile.ts
+++ src/collections/ProductFile.ts
@@ -2,54 +2,86 @@ import { User } from "../payload-types";
 import { BeforeChangeHook } from "payload/dist/collections/config/types";
 import { Access, CollectionConfig } from "payload/types";
 
+const addUser: BeforeChangeHook = ({ req, data }) => {
+  const user = req.user as User | null;
+  return { ...data, user: user?.id };
+};
 
+const yourOwnAndPurchased: Access = async ({ req }) => {
+  const user = req.user as User | null;
 
-const addUser:BeforeChangeHook = ({req,data})=>{
-    const user = req.user as User | null
-    return {...data, user: user?.id}
-}
+  if (user?.role === "admin") return true;
+  if (!user) return false;
 
-const yourOwnAndPurchased :Access =async ({req}) => {
-    const user = req.user as User | null
-
-    if(user?.role === "admin")return true
-    if(!user) return false
-
-    const  {} = await req.payload.find({
-        collection:"products" 
-    })
-}
-
-export const ProductFiles : CollectionConfig = {
-    slug:"product_files",
-    admin:{
-        hidden: ({user})=> user.role !== "admin",
-    },
-    hooks:{
-        beforeChange:[addUser],
-    },
-    access:{
-        read:yourOwnAndPurchased
-    },
-    upload: {
-        staticURL: '/product_files',
-        staticDir: 'product_files',
-        mimeTypes: [
-          'image/*',
-          'font/*',
-          'application/postscript',
-        ],
+  const { docs: products } = await req.payload.find({
+    collection: "products",
+    depth: 0, //it is just take id, if 1 whoel user
+    where: {
+      user: {
+        equals: user.id,
       },
-      fields: [
-        {
-          name: 'user',
-          type: 'relationship',
-          relationTo: 'users',
-          admin: {
-            condition: () => false,
-          },
-          hasMany: false,
-          required: true,
-        },
-    ]
-}
+    },
+  });
+
+  const ownProductFileIds = products.map((prod) => prod.product_files).flat();
+
+  const { docs: orders } = await req.payload.find({
+    collection: "orders",
+    depth: 2,
+    where: {
+      user: {
+        equals: user.id,
+      },
+    },
+  });
+
+  const purchasedProductFileIds = orders.map((order) => {
+    return order.products.map((product) => {
+      if (typeof product === "string")
+        return req.payload.logger.error(
+          "Search depth not sufficient to find pruchased IDs."
+        );
+      return typeof product.product_files === "string"
+        ? product.product_files
+        : product.product_files.id;
+    });
+  }).filter(Boolean).flat();
+
+  return{
+    id:{
+      in:[...ownProductFileIds,...purchasedProductFileIds]
+    }
+  }
+};
+
+export const ProductFiles: CollectionConfig = {
+  slug: "product_files",
+  admin: {
+    hidden: ({ user }) => user.role !== "admin",
+  },
+  hooks: {
+    beforeChange: [addUser],
+  },
+  access: {
+    read: yourOwnAndPurchased,
+    update:({req}) => req.user.role === "admin",
+    delete:({req}) => req.user.role === "admin",
+  },
+  upload: {
+    staticURL: "/product_files",
+    staticDir: "product_files",
+    mimeTypes: ["image/*", "font/*", "application/postscript"],
+  },
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      admin: {
+        condition: () => false,
+      },
+      hasMany: false,
+      required: true,
+    },
+  ],
+};
