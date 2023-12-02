a50c893d3c47c8c8fa95e8e83f925b6f18d9a191
Petr Kroutil <pkroutil1@seznam.cz>
2023-12-02 16:43:50 +0100

    dist

---
+++ dist/config/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PRODUCT_CATEGORIES = void 0;
+exports.PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui_kits",
+        featured: [
+            {
+                Name: "Editor picks",
+                href: "#",
+                imageSrc: "/nav/ui-kits/mixed.jpg"
+            },
+            {
+                Name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/ui-kits/blue.jpg"
+            },
+            {
+                Name: "Bestsellers",
+                href: "#",
+                imageSrc: "/nav/ui-kits/purple.jpg"
+            }
+        ]
+    },
+    {
+        label: "Icons",
+        value: "icons",
+        featured: [
+            {
+                Name: "Favorite Icons Picks",
+                href: "#",
+                imageSrc: "/nav/icons/picks.jpg"
+            },
+            {
+                Name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/icons/new.jpg"
+            },
+            {
+                Name: "Bestselling Icons",
+                href: "#",
+                imageSrc: "/nav/icons/bestsellers.jpg"
+            }
+        ]
+    },
+];
--- src/server.ts
+++ src/server.ts
@@ -53,6 +53,7 @@ const start = async () => {
 
       process.exit();
     });
+    return
   }
 
   app.use(
