069492f8f52cb3c2b7fe9952e7a3b433bcca1076
Petr Kroutil <pkroutil1@seznam.cz>
2023-12-02 20:30:11 +0100

    fix

--- src/components/PaymentStatus.tsx
+++ src/components/PaymentStatus.tsx
@@ -1,39 +1,57 @@
-"use client";
-import { trpc } from "@/trpc/client";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+'use client'
 
-type PaymentStatusProps = {
-  orderEmail: string;
-  orderId: string;
-  isPaid: boolean;
-};
+import { trpc } from '@/trpc/client'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
-const PaymentStatus = ({ orderEmail, orderId, isPaid }: PaymentStatusProps) => {
-    const router = useRouter()
-  const {data} = trpc.payment.pollOrderStatus.useQuery({
-    orderId},{
-        enabled: isPaid === false,
-        refetchInterval: (data)=> data?.isPaid ? false : 1000       //make req ervy 1sec to check if order is paid
-    });
+interface PaymentStatusProps {
+  orderEmail: string
+  orderId: string
+  isPaid: boolean
+}
 
+const PaymentStatus = ({
+  orderEmail,
+  orderId,
+  isPaid,
+}: PaymentStatusProps) => {
+  const router = useRouter()
 
-    useEffect(() => {
-      if(data?.isPaid)router.refresh()
-    }, [data?.isPaid,router])
-    
+  const { data } = trpc.payment.pollOrderStatus.useQuery(
+    { orderId },
+    {
+      enabled: isPaid === false,
+      refetchInterval: (data) =>
+        data?.isPaid ? false : 1000,
+    }
+    //make req  1sec to check if order is paid
+  )
+
+  useEffect(() => {
+    if (data?.isPaid) router.refresh()
+  }, [data?.isPaid, router])
+
   return (
-    <div className="mt-16 grid grid-cols-2 gap-x-4 text-sm text-gray-600">
+    <div className='mt-16 grid grid-cols-2 gap-x-4 text-sm text-gray-600'>
       <div>
-        <p className="font-medium text-gray-900">Shipping To</p>
+        <p className='font-medium text-gray-900'>
+          Shipping To
+        </p>
         <p>{orderEmail}</p>
       </div>
+
       <div>
-        <p className="font-medium text-gray-900">Order status</p>
-        <p>{isPaid ? "Payment successful" : "Pending payment"}</p>
+        <p className='font-medium text-gray-900'>
+          Order Status
+        </p>
+        <p>
+          {isPaid
+            ? 'Payment successful'
+            : 'Pending payment'}
+        </p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PaymentStatus;
+export default PaymentStatus
