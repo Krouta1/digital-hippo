3aa21e262f985677ea621378f204dda6b1b97b57
Petr Kroutil <pkroutil1@seznam.cz>
2023-12-02 19:40:50 +0100

    final changes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: 'localhost',
-            port: '3000',
-            pathname: '**',
-          },
-        ],
+        domains:[
+          "localhost",
+          "digital-hippo-production-b47c.up.railway.app"
+        ]
       },
 }
 
