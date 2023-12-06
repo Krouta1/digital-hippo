df6f6a33c794d097eebe9017143866d03c6f3b25
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2023-12-06 14:30:43 +0100

    Update Products.ts

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,94 +1,109 @@
-import { AfterChangeHook, BeforeChangeHook } from "payload/dist/collections/config/types";
-import { PRODUCT_CATEGORIES } from "../../config";
-import { Access, CollectionConfig } from "payload/types";
-import { Product, User } from "../../payload-types";
-import { stripe } from "../../lib/stripe";
+import {
+  AfterChangeHook,
+  BeforeChangeHook,
+} from 'payload/dist/collections/config/types'
+import { PRODUCT_CATEGORIES } from '../../config'
+import { Access, CollectionConfig } from 'payload/types'
+import { Product, User } from '../../payload-types'
+import { stripe } from '../../lib/stripe'
 
-const addUser :BeforeChangeHook<Product> =async ({req,data}) => {
+const addUser: BeforeChangeHook<Product> = async ({
+  req,
+  data,
+}) => {
   const user = req.user
 
-  return{...data,user:user.id}
+  return { ...data, user: user.id }
 }
 
-const syncUser: AfterChangeHook <Product> = async ({req,doc}) =>{
+const syncUser: AfterChangeHook<Product> = async ({
+  req,
+  doc,
+}) => {
   const fullUser = await req.payload.findByID({
-    collection:"users",
-    id:req.user.id,
+    collection: 'users',
+    id: req.user.id,
   })
 
-  if(fullUser && typeof fullUser === "object"){
-    const {products} = fullUser
+  if (fullUser && typeof fullUser === 'object') {
+    const { products } = fullUser
 
-    const allIDS = [
-      ...(products?.map((product)=> typeof product === "object"? product.id : product) || []) 
+    const allIDs = [
+      ...(products?.map((product) =>
+        typeof product === 'object' ? product.id : product
+      ) || []),
     ]
 
-    const createdProductIDs = allIDS.filter((id,index) => allIDS.indexOf(id) === index)
+    const createdProductIDs = allIDs.filter(
+      (id, index) => allIDs.indexOf(id) === index
+    )
 
     const dataToUpdate = [...createdProductIDs, doc.id]
 
     await req.payload.update({
-      collection:"users",
+      collection: 'users',
       id: fullUser.id,
-      data:{
+      data: {
         products: dataToUpdate,
-      }
+      },
     })
   }
 }
 
-const isAdminOrHasAccess = ():Access =>({req:{user:_user}})=>{
-  const user = _user as User | undefined
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined
 
-  if(!user) return false
-  if(user.role === "admin") return true
+    if (!user) return false
+    if (user.role === 'admin') return true
 
-  const userProductIDs = (user.products || []).reduce<Array<string>>((acc,product)=>{
-    if(!product) return acc
+    const userProductIDs = (user.products || []).reduce<
+      Array<string>
+    >((acc, product) => {
+      if (!product) return acc
+      if (typeof product === 'string') {
+        acc.push(product)
+      } else {
+        acc.push(product.id)
+      }
 
-    if(typeof product === "string"){
-      acc.push(product)
-    }else{
-      acc.push(product.id)
-    }
-    return acc
-  },[])
+      return acc
+    }, [])
 
-  return {
-    id:{
-      in: userProductIDs
+    return {
+      id: {
+        in: userProductIDs,
+      },
     }
   }
-}
-
 
 export const Products: CollectionConfig = {
-  slug: "products",
+  slug: 'products',
   admin: {
-    useAsTitle: "name",
+    useAsTitle: 'name',
   },
   access: {
-    read:isAdminOrHasAccess(),
+    read: isAdminOrHasAccess(),
     update: isAdminOrHasAccess(),
     delete: isAdminOrHasAccess(),
   },
-  hooks:{
-    afterChange:[syncUser],
-    beforeChange:[
+  hooks: {
+    afterChange: [syncUser],
+    beforeChange: [
       addUser,
-
-      //this one creating all the fields fro stripe (stripeId and priceId)
       async (args) => {
-        if(args.operation === "create"){
+        if (args.operation === 'create') {
           const data = args.data as Product
 
-          const createdProduct = await stripe.products.create({
-            name:data.name,
-            default_price_data:{
-              currency: "USD",
-              unit_amount: Math.round(data.price * 100), // price was in cents !!!
-            },
-          })
+          const createdProduct =
+            await stripe.products.create({
+              name: data.name,
+              default_price_data: {
+                currency: 'USD',
+                unit_amount: Math.round(data.price * 100),
+              },
+            })
 
           const updated: Product = {
             ...data,
@@ -97,12 +112,14 @@ export const Products: CollectionConfig = {
           }
 
           return updated
-        }else if(args.operation === "update"){
+        } else if (args.operation === 'update') {
           const data = args.data as Product
-          const updatedProduct = await stripe.products.update(data.stripeId!,{
-            name:data.name,
-            default_price: data.priceId!,
-          })
+
+          const updatedProduct =
+            await stripe.products.update(data.stripeId!, {
+              name: data.name,
+              default_price: data.priceId!,
+            })
 
           const updated: Product = {
             ...data,
@@ -113,7 +130,7 @@ export const Products: CollectionConfig = {
           return updated
         }
       },
-    ]
+    ],
   },
   fields: [
     {
