d1247a3d1a0188b44614f0b80a05e52836a5b9db
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2023-12-06 13:55:33 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import express from "express";
-import { getPayloadClient } from "./get-payload";
-import { nextApp, nextHandler } from "./next-utils";
-import * as trpcExpress from "@trpc/server/adapters/express";
-import { appRouter } from "./trpc";
-import { inferAsyncReturnType } from "@trpc/server";
-import bodyParser from "body-parser";
-import nextBuild from "next/dist/build";
-import { IncomingMessage } from "http";
-import { stripeWebhookHandler } from "./webhooks";
-import path from "path";
-import { PayloadRequest } from "payload/types";
-import { parse } from "url";
+import express from 'express'
+import { getPayloadClient } from './get-payload'
+import { nextApp, nextHandler } from './next-utils'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { appRouter } from './trpc'
+import { inferAsyncReturnType } from '@trpc/server'
+import bodyParser from 'body-parser'
+import { IncomingMessage } from 'http'
+import { stripeWebhookHandler } from './webhooks'
+import nextBuild from 'next/dist/build'
+import path from 'path'
+import { PayloadRequest } from 'payload/types'
+import { parse } from 'url'
 
-const app = express();
-const PORT = Number(process.env.PORT) || 3000;
+const app = express()
+const PORT = Number(process.env.PORT) || 3000
 
 const createContext = ({
   req,
@@ -21,21 +21,30 @@ const createContext = ({
 }: trpcExpress.CreateExpressContextOptions) => ({
   req,
   res,
-});
+})
 
 //this to get types of context, very usefull whe you want to get something like token from context :)
-export type ExpressContext = inferAsyncReturnType<typeof createContext>;
-export type WebhookRequest = IncomingMessage & { rawBody: Buffer };
+export type ExpressContext = inferAsyncReturnType<
+  typeof createContext
+>
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer
+}
 
 const start = async () => {
   //stripe webhook
-  const webHookMiddleware = bodyParser.json({
+  const webhookMiddleware = bodyParser.json({
     verify: (req: WebhookRequest, _, buffer) => {
-      req.rawBody = buffer;
+      req.rawBody = buffer
     },
-  });
+  })
   //also for stripe
-  app.post("/api/webhooks/stripe", webHookMiddleware, stripeWebhookHandler);
+  app.post(
+    '/api/webhooks/stripe',
+    webhookMiddleware,
+    stripeWebhookHandler
+  )
+
 
   const payload = await getPayloadClient({
     initOptions: {
